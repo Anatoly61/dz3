e76dc333f281b2ee340a35c63600fbf8dfee36f4
Anatoly Epifanov <rgau61@gmail.com>
2023-06-26 13:33:20 +0300

    new fork

---
+++ task.js
@@ -0,0 +1,10 @@
+function compareArrays(arr1, arr2) {
+    let result;
+
+  return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]); 
+}
+
+function getUsersNamesInAgeRange(users, gender) {
+    users.filter(user=>user.gender ==="Мужской");
+    let averageage = people.reduce((accumulator, people) => (accumulator + people.age)/length,0);
+}
