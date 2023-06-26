28a4ca4d8fa0a8df3adcb223309de6ce4b29fe54
Anatoly Epifanov <rgau61@gmail.com>
2023-06-26 14:43:48 +0300

    Исправление от 26.06.2023

--- task.js
+++ task.js
@@ -1,10 +1,13 @@
 function compareArrays(arr1, arr2) {
-    let result;
 
   return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]); 
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-    users.filter(user=>user.gender ==="Мужской");
-    let averageage = people.reduce((accumulator, people) => (accumulator + people.age)/length,0);
-}
+  
+  let mas = users.filter(item => item.gender === gender).map(item => item.age);
+  let res = mas.reduce((sum, age) => sum + age, 0);
+  
+  return (mas.length > 0) ? res / mas.length : 0;
+  
+  }
